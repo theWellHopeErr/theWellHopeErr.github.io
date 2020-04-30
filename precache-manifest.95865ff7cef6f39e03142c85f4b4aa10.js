0de5f13cb2e55714fc7d6726a498a4b6e0677254
theWellHopeErr <ssuryarajan@gmail.com>
2020-04-30 12:30:47 +0530

    Updates

---
+++ precache-manifest.95865ff7cef6f39e03142c85f4b4aa10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fe72126c8a81ea3439588f62fec7410",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a159823e1cb4f433c258",
+    "url": "/static/css/main.a08e7a67.chunk.css"
+  },
+  {
+    "revision": "643863a113655b7d009d",
+    "url": "/static/js/2.5fb6b614.chunk.js"
+  },
+  {
+    "revision": "6c287a6cf2599d40f51a03ba812d8a6e",
+    "url": "/static/js/2.5fb6b614.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a159823e1cb4f433c258",
+    "url": "/static/js/main.674fb681.chunk.js"
+  },
+  {
+    "revision": "8601b179c189311c6d61",
+    "url": "/static/js/runtime-main.5ffcbc3c.js"
+  }
+]);
